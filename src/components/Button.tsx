aa90f188cb0cd1637c88631159e4f37bc17c4c89
MuriloZague <zaguemurilo7@gmail.com>
2025-01-20 13:37:37 -0300

    feat: button and dvd upgrade

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,34 +2,37 @@ import { useClicks } from "../utils/ClicksContext";
 import { useItems } from "../utils/ItemsContext";
 
 export default function Button() {
+  const { clicks, manualClick } = useClicks();
+  const { items } = useItems();
 
-    const { clicks, manualClick } = useClicks();
-    const { items } = useItems();
+  const buttonUpgrade = items.find((item) => item.id === 3);
+  const upgradeCheker = buttonUpgrade?.quantity === -1;
 
-    const buttonUpgrade = items.find((item) => item.id === 3);
-    const upgradeCheker = buttonUpgrade?.quantity === -1
-
-    return (
-
-        <div className="flex flex-col items-center mb-10">
-                <div className={upgradeCheker ? "flex-button-container" : ""}>
-                    <div className={upgradeCheker ? "neo-button" : ""} onClick={manualClick}>
-                        <button className="bg-[#f0f0f0] border buttonTrans hover:bg-[#ddd] border-[#777] p-1 px-4">
-                            <a className="select-none inter-tight text-[1.35rem]">Clique</a>
-                        </button>
-                    </div>
-                </div>
-            <div className="text-center mt-5">
-                {clicks === 0 ?
-                    <p className="select-none inter-tight opacity-0 text-2xl">
-                        {clicks}
-                    </p>
-                    :
-                    <p className="inter-tight transition-opacity duration-500 text-2xl">
-                        {clicks} estímulos
-                    </p>
-                }
-            </div>
+  return (
+    <div className="flex flex-col items-center mb-10">
+      <div
+        className={`flex-button-container ${upgradeCheker ? "neo-button-container" : "h-16 flex items-center justify-center"}`}
+        style={{ borderRadius: '15px' }}
+        onClick={manualClick}
+      >
+        <div className={upgradeCheker ? "neo-button" : ""}>
+          <button className={`bg-[#f0f0f0] border buttonTrans ${upgradeCheker ? "" : "hover:bg-[#d6d6d6]"} border-[#777] p-1 px-4`}>
+            <a className="select-none inter-tight text-[1.35rem]">Clique</a>
+          </button>
         </div>
-    );
+      </div>
+
+      <div className="text-center mt-5">
+        {clicks === 0 ? (
+          <p className="select-none inter-tight opacity-0 text-2xl">
+            {clicks}
+          </p>
+        ) : (
+          <p className="inter-tight transition-opacity duration-500 text-2xl">
+            {clicks} estímulos
+          </p>
+        )}
+      </div>
+    </div>
+  );
 }
